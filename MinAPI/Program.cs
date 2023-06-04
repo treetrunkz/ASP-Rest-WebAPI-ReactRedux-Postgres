37f5ff41d6f8976b27f3433de51eb89c57031436
treetrunkz <ecarl@mail.greenriver.edu>
2023-06-03 20:29:30 -0700

    Connect PostgreSQL with Entity Framework ORM

--- MinAPI/Program.cs
+++ MinAPI/Program.cs
@@ -1,7 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Logging.AddJsonConsole();
+
+// Connect to PostgreSQL Database
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<NoteDb>(options =>
+    options.UseNpgsql(connectionString));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Welcome to Notes API!");
 
 await app.RunAsync();
+
+record Note(int id){
+    public string text {get;set;} = default!;
+    public bool done {get;set;} = default!;
+}
+
+using Microsoft.EntityFrameworkCore; // place this line at the beginning of file.
+
+class NoteDb: DbContext {
+    public NoteDb(DbContextOptions<NoteDb> options): base(options) {
+
+    }
+    public DbSet<Note> Notes => Set<Note>();
+}
